779f8e6551ce79d0bd51f1a14dfb31d3f1b1d538
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-10-28 13:29:56 +0900

    test rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,5 @@
-extern crate wasm_bindgen;
-use wasm_bindgen::prelude::*;
-// JavaScript によって提供される alert 関数の定義
-#[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+#[no_mangle]
 
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
-// JavaScript が呼び出せる Rust 関数
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-pub fn output_log(name: &str) {
-    log(&format!("Hello {}!!!", name))
+pub fn twice(x: i32) -> i32 {
+    x << 1
 }
